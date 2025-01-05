85fa8aa457b7226c7dee5034d1b42c9ca8419d1a
Dipankar Mitra <dipankar9612@gmail.com>
2025-01-06 01:29:00 +0530

    added model
    replaced hard-coded dictionary from views to database model.

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author, Tag, Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "tags","date")
+    list_display = ("title", "date", "author")
+
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag)
--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+
 
 # Create your models here.
+
+class Tag(models.Model):
+    captions = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.captions
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField()
+
+    def full_name_with_email(self):
+        return f"{self.first_name} {self.last_name} ({self.email})"
+
+    def __str__(self):
+        return self.full_name_with_email()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=300)
+    image_name = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts", null=True)
+    tags = models.ManyToManyField(Tag)
+
+
+    def __str__(self):
+        return f"{self.title} on {self.date}"
+
+    ## This method is handled in admin.py
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.title)
+    #     super().save(*args, *kwargs)
